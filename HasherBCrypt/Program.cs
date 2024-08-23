dc6aba611eabe12a826c5e15959f4e6dddb43908
Richard Soares <108972853+RickSRS@users.noreply.github.com>
2024-08-23 16:54:34 -0300

    Adicionar hash e validação de senha bcrypt
    Este commit apresenta um novo aplicativo de console .NET 8.0 para hash e validação de senhas usando BCrypt. Todos os arquivos de projeto necessários, definições de configuração e dependências como "BCrypt.Net-Next" estão incluídos.

---
+++ HasherBCrypt/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace HasherBCrypt
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Digite a senha a ser hasheada:");
+            var password = Console.ReadLine();
+
+            // Gera o hash da senha
+            var hash = BCryptHasher.HashPassword(password);
+
+            // Exibe o hash gerado
+            Console.WriteLine($"Hash gerado: {hash}\n");
+
+            Console.WriteLine("Digite a senha novamente para validação:");
+            var passwordToValidate = Console.ReadLine();
+
+            // Valida a senha
+            var isValid = BCryptHasher.ValidatePassword(passwordToValidate, hash);
+
+            // Exibe o resultado da validação
+            Console.WriteLine($"Senha válida: {isValid}");
+        }
+    }
+
+    public static class BCryptHasher
+    {
+        /// <summary>
+        /// Gera um hash para a senha fornecida.
+        /// </summary>
+        /// <param name="password">A senha a ser hashed.</param>
+        /// <returns>O hash gerado.</returns>
+        public static string HashPassword(string? password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        /// <summary>
+        /// Valida uma senha fornecida em comparação com um hash armazenado.
+        /// </summary>
+        /// <param name="password">A senha fornecida para validação.</param>
+        /// <param name="hash">O hash armazenado.</param>
+        /// <returns>True se a senha for válida, caso contrário, false.</returns>
+        public static bool ValidatePassword(string? password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
