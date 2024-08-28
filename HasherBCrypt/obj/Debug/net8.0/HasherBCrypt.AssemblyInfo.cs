7b4c55c45b71d894d2c7939162ad2170be4a2331
Richard Soares <108972853+RickSRS@users.noreply.github.com>
2024-08-27 22:16:26 -0300

    Update project paths from DOT NET to C#
    Updated various configuration and asset files to reflect the new project path from DOT NET to C#. This change ensures consistency across all related project and configuration files.

--- HasherBCrypt/obj/Debug/net8.0/HasherBCrypt.AssemblyInfo.cs
+++ HasherBCrypt/obj/Debug/net8.0/HasherBCrypt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HasherBCrypt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6830083f032f2fa028e9cf4b4b2f4c767ee38ecc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c466e58a9b1a607ae228d06ae55d123be8c065dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HasherBCrypt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HasherBCrypt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
